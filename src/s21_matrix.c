5d273806775c4f69d9e02169eb0df89aa499e450
Nikolai <dolaxom4@gmail.com>
2022-08-27 01:19:14 +0300

    [eq_matrix] Adding equal function

--- src/s21_matrix.c
+++ src/s21_matrix.c
@@ -38,3 +38,17 @@ void s21_remove_matrix(matrix_t *A) {
 
     free(A->matrix);
 }
+
+int s21_eq_matrix(matrix_t *A, matrix_t *B) {
+    if (A->rows != B->rows || A->columns != B->columns) {
+        return FAILURE;
+    }
+
+    for (int i = 0; i < A->rows; i++)
+        for (int j = 0; j < A->columns; j++)
+            if (A->matrix[i][j] != B->matrix[i][j])
+                return FAILURE;
+
+
+    return SUCCESS;
+}
