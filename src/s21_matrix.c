3ec874e112c69f51195f6b3bf8acea396f59165d
Nikolai <dolaxom4@gmail.com>
2022-08-27 01:17:45 +0300

    [init] Adding base files

---
+++ src/s21_matrix.c
@@ -0,0 +1,1 @@
+#include "s21_matrix.h"
---
+++ src/s21_matrix.h
@@ -0,0 +1,43 @@
+#ifndef S21_MATRIX_S21_MATRIX_H
+#define S21_MATRIX_S21_MATRIX_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SUCCESS 1
+#define FAILURE 0
+
+enum {OK, MATRIX_ERROR, CALC_ERROR};
+
+typedef struct matrix_struct {
+    double** matrix;
+    int rows;
+    int columns;
+} matrix_t;
+
+//main
+int s21_create_matrix(int rows, int columns, matrix_t *result);
+void s21_remove_matrix(matrix_t *A);
+
+int s21_eq_matrix(matrix_t *A, matrix_t *B);
+
+int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+
+int s21_mult_number(matrix_t *A, double number, matrix_t *result);
+int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+
+int s21_transpose(matrix_t *A, matrix_t *result);
+
+int s21_calc_complements(matrix_t *A, matrix_t *result);
+
+int s21_determinant(matrix_t *A, double *result);
+
+int s21_inverse_matrix(matrix_t *A, matrix_t *result);
+
+// helpers
+int s21_init_matrix(int rows, int columns, matrix_t *result);
+const int s21_output_matrix(const matrix_t *result);
+
+
+#endif //S21_MATRIX_S21_MATRIX_H
