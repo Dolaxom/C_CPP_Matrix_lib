12b463998f8cb28e36bbce1ffa6cefe59aeeb776
TheDolaxom <51160711+Dolaxom@users.noreply.github.com>
2022-08-27 19:36:56 +0300

    [mult_matrix/num][transpose] Adding new functions

--- src/s21_matrix.c
+++ src/s21_matrix.c
@@ -40,9 +40,8 @@ void s21_remove_matrix(matrix_t *A) {
 }
 
 int s21_eq_matrix(matrix_t *A, matrix_t *B) {
-    if (A->rows != B->rows || A->columns != B->columns) {
+    if (A->rows != B->rows || A->columns != B->columns)
         return FAILURE;
-    }
 
     for (int i = 0; i < A->rows; i++)
         for (int j = 0; j < A->columns; j++)
@@ -52,3 +51,73 @@ int s21_eq_matrix(matrix_t *A, matrix_t *B) {
 
     return SUCCESS;
 }
+
+int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
+    if (A->rows != B->rows || A->columns != B->columns)
+        return CALC_ERROR;
+
+    s21_create_matrix(A->rows, A->columns, result);
+
+    for (int i = 0; i < result->rows; i++)
+        for (int j = 0; j < result->columns; j++)
+            result->matrix[i][j] = A->matrix[i][j] + B->matrix[i][j];
+
+    return OK;
+}
+
+int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
+    if (A->rows != B->rows || A->columns != B->columns)
+        return CALC_ERROR;
+
+    s21_create_matrix(A->rows, A->columns, result);
+
+    for (int i = 0; i < result->rows; i++)
+        for (int j = 0; j < result->columns; j++)
+            result->matrix[i][j] = A->matrix[i][j] - B->matrix[i][j];
+
+    return OK;
+}
+
+int s21_mult_number(matrix_t *A, double number, matrix_t *result) {
+    if (A->rows != result->rows || A->columns != result->columns)
+        return CALC_ERROR;
+
+    s21_create_matrix(A->rows, A->columns, result);
+
+    for (int i = 0; i < result->rows; i++)
+        for (int j = 0; j < result->columns; j++)
+            result->matrix[i][j] = A->matrix[i][j] * number;
+
+    return OK;
+}
+
+int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
+    int max_row = B->rows;
+    int max_col = B->columns;
+    if (A->rows > B->rows)
+        max_row = A->rows;
+    if (A->columns > B->columns)
+        max_col = A->columns;
+
+    s21_create_matrix(max_row, max_col, result);
+
+    for (int i = 0; i < A->rows; i++)
+        for (int j = 0; j < B->columns; j++)
+            for (int k = 0; k < B->rows; k++)
+                result->matrix[i][j] += A->matrix[i][k] * B->matrix[k][j];
+
+    return OK;
+}
+
+int s21_transpose(matrix_t *A, matrix_t *result) {
+    s21_create_matrix(A->columns, A->rows, result);
+    
+    for (int i = 0; i < result->rows; i++) {
+        for (int j = 0; j < result->columns; j++) {
+            result->matrix[i][j] = A->matrix[j][i];
+        }
+    }
+
+    return OK;
+}
+
