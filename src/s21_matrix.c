e08f2f92e5fef8bec0741eeb1898030f34a30bc9
TheDolaxom <51160711+Dolaxom@users.noreply.github.com>
2022-09-07 18:17:15 +0300

    [WIP][s21_calc_complements][s21_minor_matrix]
    Adding based functions for s21_calc_complements like s21_minor_matrix && s21_determinant_simple2x2

--- src/s21_matrix.c
+++ src/s21_matrix.c
@@ -1,123 +1,152 @@
 #include "s21_matrix.h"
 
 int s21_create_matrix(int rows, int columns, matrix_t *result) {
-    result->rows = rows;
-    result->columns = columns;
+  result->rows = rows;
+  result->columns = columns;
 
-    result->matrix = (double **) malloc(rows * sizeof(double*));
-    for (int i = 0; i < rows; i++) {
-        result->matrix[i] = (double *) malloc(columns * sizeof(double));
-    }
+  result->matrix = (double **)malloc(rows * sizeof(double *));
+  for (int i = 0; i < rows; i++) {
+    result->matrix[i] = (double *)malloc(columns * sizeof(double));
+  }
 
-    s21_init_matrix(rows, columns, result);
+  s21_init_matrix(rows, columns, result);
 
-    return 0;
-}
-
-const int s21_output_matrix(const matrix_t *result) {
-    for (int i = 0; i < result->rows; i++) {
-        for (int j = 0; j < result->columns; j++)
-            printf("%f ", result->matrix[i][j]);
-        printf("\n");
-    }
-
-    return 0;
-}
-
-int s21_init_matrix(int rows, int columns, matrix_t *result) {
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < columns; j++)
-            result->matrix[i][j] = 0.0;
-
-    return 0;
+  return 0;
 }
 
 void s21_remove_matrix(matrix_t *A) {
-    for (int i = 0; i < A->rows; i++)
-        free(A->matrix[i]);
+  for (int i = 0; i < A->rows; i++) free(A->matrix[i]);
 
-    free(A->matrix);
+  free(A->matrix);
 }
 
 int s21_eq_matrix(matrix_t *A, matrix_t *B) {
-    if (A->rows != B->rows || A->columns != B->columns)
-        return FAILURE;
+  if (A->rows != B->rows || A->columns != B->columns) return FAILURE;
 
-    for (int i = 0; i < A->rows; i++)
-        for (int j = 0; j < A->columns; j++)
-            if (A->matrix[i][j] != B->matrix[i][j])
-                return FAILURE;
+  for (int i = 0; i < A->rows; i++)
+    for (int j = 0; j < A->columns; j++)
+      if (A->matrix[i][j] != B->matrix[i][j]) return FAILURE;
 
-
-    return SUCCESS;
+  return SUCCESS;
 }
 
 int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
-    if (A->rows != B->rows || A->columns != B->columns)
-        return CALC_ERROR;
+  if (A->rows != B->rows || A->columns != B->columns) return CALC_ERROR;
 
-    s21_create_matrix(A->rows, A->columns, result);
+  s21_create_matrix(A->rows, A->columns, result);
 
-    for (int i = 0; i < result->rows; i++)
-        for (int j = 0; j < result->columns; j++)
-            result->matrix[i][j] = A->matrix[i][j] + B->matrix[i][j];
+  for (int i = 0; i < result->rows; i++)
+    for (int j = 0; j < result->columns; j++)
+      result->matrix[i][j] = A->matrix[i][j] + B->matrix[i][j];
 
-    return OK;
+  return OK;
 }
 
 int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
-    if (A->rows != B->rows || A->columns != B->columns)
-        return CALC_ERROR;
+  if (A->rows != B->rows || A->columns != B->columns) return CALC_ERROR;
 
-    s21_create_matrix(A->rows, A->columns, result);
+  s21_create_matrix(A->rows, A->columns, result);
 
-    for (int i = 0; i < result->rows; i++)
-        for (int j = 0; j < result->columns; j++)
-            result->matrix[i][j] = A->matrix[i][j] - B->matrix[i][j];
+  for (int i = 0; i < result->rows; i++)
+    for (int j = 0; j < result->columns; j++)
+      result->matrix[i][j] = A->matrix[i][j] - B->matrix[i][j];
 
-    return OK;
+  return OK;
 }
 
 int s21_mult_number(matrix_t *A, double number, matrix_t *result) {
-    if (A->rows != result->rows || A->columns != result->columns)
-        return CALC_ERROR;
+  s21_create_matrix(A->rows, A->columns, result);
 
-    s21_create_matrix(A->rows, A->columns, result);
+  for (int i = 0; i < result->rows; i++)
+    for (int j = 0; j < result->columns; j++)
+      result->matrix[i][j] = A->matrix[i][j] * number;
 
-    for (int i = 0; i < result->rows; i++)
-        for (int j = 0; j < result->columns; j++)
-            result->matrix[i][j] = A->matrix[i][j] * number;
-
-    return OK;
+  return OK;
 }
 
 int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
-    int max_row = B->rows;
-    int max_col = B->columns;
-    if (A->rows > B->rows)
-        max_row = A->rows;
-    if (A->columns > B->columns)
-        max_col = A->columns;
+  int max_row = B->rows;
+  int max_col = B->columns;
+  if (A->rows > B->rows) max_row = A->rows;
+  if (A->columns > B->columns) max_col = A->columns;
 
-    s21_create_matrix(max_row, max_col, result);
+  s21_create_matrix(max_row, max_col, result);
 
-    for (int i = 0; i < A->rows; i++)
-        for (int j = 0; j < B->columns; j++)
-            for (int k = 0; k < B->rows; k++)
-                result->matrix[i][j] += A->matrix[i][k] * B->matrix[k][j];
+  for (int i = 0; i < A->rows; i++)
+    for (int j = 0; j < B->columns; j++)
+      for (int k = 0; k < B->rows; k++)
+        result->matrix[i][j] += A->matrix[i][k] * B->matrix[k][j];
 
-    return OK;
+  return OK;
 }
 
 int s21_transpose(matrix_t *A, matrix_t *result) {
-    s21_create_matrix(A->columns, A->rows, result);
-    
-    for (int i = 0; i < result->rows; i++) {
-        for (int j = 0; j < result->columns; j++) {
-            result->matrix[i][j] = A->matrix[j][i];
-        }
-    }
+  s21_create_matrix(A->columns, A->rows, result);
 
-    return OK;
+  for (int i = 0; i < result->rows; i++) {
+    for (int j = 0; j < result->columns; j++) {
+      result->matrix[i][j] = A->matrix[j][i];
+    }
+  }
+
+  return OK;
 }
 
+int s21_calc_complements(matrix_t *A, matrix_t *result) {
+  s21_create_matrix(A->rows, A->columns, result);
+  matrix_t buffer = *result;
+  double det;
+  for (int i = 0; i < A->rows; i++) {
+    for (int j = 0; j < A->columns; j++) {
+      s21_minor_matrix(A, &buffer, i, j);
+      det = s21_determinant_simple2x2(&buffer, &det);
+      result->matrix[i][j] = det * pow(-1, (i + j));
+    }
+  }
+
+  return OK;
+}
+
+/*
+ * HELPERS
+ */
+
+int s21_init_matrix(int rows, int columns, matrix_t *result) {
+  for (int i = 0; i < rows; i++)
+    for (int j = 0; j < columns; j++) result->matrix[i][j] = 0.0;
+
+  return 0;
+}
+
+void s21_output_matrix(const matrix_t result) {
+  for (int i = 0; i < result.rows; i++) {
+    for (int j = 0; j < result.columns; j++) printf("%f ", result.matrix[i][j]);
+    printf("\n");
+  }
+}
+
+int s21_minor_matrix(matrix_t *A, matrix_t *result, int rows, int columns) {
+  int result_rows = A->rows - 1;
+  int result_columns = A->columns - 1;
+  s21_create_matrix(result_rows, result_columns, result);
+  result_rows = 0;
+  result_columns = 0;
+  for (int i = 0; i < A->rows; i++) {
+    for (int j = 0; j < A->columns; j++) {
+      if (i == rows || j == columns) continue;
+
+      result->matrix[result_rows][result_columns] = A->matrix[i][j];
+      result_columns++;
+    }
+    // Если строчка не была пустой, то мы идем на следующую
+    if (i != rows) result_rows++;
+    result_columns = 0;
+  }
+
+  return OK;
+}
+
+int s21_determinant_simple2x2(matrix_t *A, double *result) {
+  return *result = A->matrix[0][0] * A->matrix[1][1] -
+                   A->matrix[0][1] * A->matrix[1][0];
+}
