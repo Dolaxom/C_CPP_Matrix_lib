9558f01d8f3cecc4fc006589cce782149bdacbad
TheDolaxom <51160711+Dolaxom@users.noreply.github.com>
2022-09-08 17:07:57 +0300

    [s21_inverse_matrix]

--- src/s21_matrix.c
+++ src/s21_matrix.c
@@ -105,21 +105,60 @@ int s21_calc_complements(matrix_t *A, matrix_t *result) {
 }
 
 int s21_determinant(matrix_t *A, double *result) {
-  matrix_t buffer_1;
-  matrix_t buffer_2;
-  s21_create_matrix(A->rows, A->columns, &buffer_1);
-  s21_create_matrix(A->rows, A->columns, &buffer_2);
-  double det = 0.0;
+  matrix_t buffer;
+  s21_create_matrix(A->rows, A->columns, &buffer);
   *result = 0.0;
-  for (int j = 0; j < A->columns; j++) {
-    s21_chess_sign_matrix(*A, &buffer_1);
-    s21_minor_element(A, &buffer_2, 0, j);
-    det = s21_determinant_simple2x2(&buffer_2, &det);
-    *result += buffer_1.matrix[0][j] * det;
+  if (A->rows == 1) {
+    *result = A->matrix[0][0];
+  } else if (A->rows == 2) {
+    s21_determinant_simple2x2(A, result);
+  } else if (A->rows == 3) {
+    s21_calc_complements(A, &buffer);
+    for (int j = 0; j < A->rows; j++)
+      *result += A->matrix[0][j] * buffer.matrix[0][j];
+  } else {
+    double det = 0;
+    for (int j = 0; j < A->rows; j++) {
+      s21_minor_element(A, &buffer, 0, j);
+      s21_determinant(&buffer, &det);
+      det *= A->matrix[0][j];
+      if (j % 2) {
+        *result -= det;
+      } else {
+        *result += det;
+      }
+    }
   }
 
-  s21_remove_matrix(&buffer_1);
-  s21_remove_matrix(&buffer_2);
+  s21_remove_matrix(&buffer);
+
+  return OK;
+}
+
+int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
+  if (A->rows == A->columns) {
+    double det = 0;
+    s21_determinant(A, &det);
+
+    if (det != 0.0) {
+      matrix_t buffer_1;
+      matrix_t buffer_2;
+
+      s21_calc_complements(A, &buffer_1);
+      printf("\n\n");
+      s21_output_matrix(buffer_1);
+      printf("\n\n");
+
+      s21_transpose(&buffer_1, &buffer_2);
+      printf("\n\n");
+      s21_output_matrix(buffer_2);
+      printf("\n\n");
+      s21_mult_number(&buffer_2, 1.0 / det, result);
+
+      s21_remove_matrix(&buffer_1);
+      s21_remove_matrix(&buffer_2);
+    }
+  }
 
   return OK;
 }
