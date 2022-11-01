d888e1775c5dc9cae94587e4136599346616989f
TheDolaxom <51160711+Dolaxom@users.noreply.github.com>
2022-11-01 18:17:47 +0300

    [main.c] Adding new main file with tests

---
+++ CPP/Matrix_2.0/src/s21_main.c
@@ -0,0 +1,22 @@
+#include "s21_matrix.h"
+
+int main() {
+    S21Matrix matrix_a(2, 2);
+    S21Matrix matrix_b(2, 2);
+    matrix_a(0, 0) = 5;
+    matrix_a(0, 1) = 2;
+    matrix_a(1, 0) = 4;
+    matrix_a(1, 1) = 3;
+
+    matrix_b(0, 0) = 1;
+    matrix_b(0, 1) = 4;
+    matrix_b(1, 0) = 5;
+    matrix_b(1, 1) = 2;
+
+//    S21Matrix matrix_result = matrix_a * matrix_b;
+    matrix_a *= matrix_b;
+
+    std::cout << "final\n";
+    matrix_a.S21OutputMatrix();
+    return 0;
+}
